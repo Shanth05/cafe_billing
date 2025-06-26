afb0eb46da6912f2d96834b076b0af58a351cf05
Subiramaniam Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-06-26 13:27:03 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +20,16 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', RoleMiddleware::class . ':admin'])->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+});
+
+Route::middleware(['auth', RoleMiddleware::class . ':admin'])->prefix('admin')->group(function () {
+    Route::get('users', [UserController::class, 'index'])->name('admin.users.index');
+    Route::post('users/{user}/role', [UserController::class, 'updateRole'])->name('admin.users.updateRole');
+});
+
 
 require __DIR__.'/auth.php';
