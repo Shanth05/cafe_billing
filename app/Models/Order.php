90aebd7afccb0dd968e526b6839ff5d823b3e78b
Subiramaniam Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-06-27 14:58:26 +0530

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,18 +1,24 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;  // enable soft deletes
 
     protected $fillable = [
         'customer_name',
         'item',
         'quantity',
         'total',
+        'user_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
