7cf9f46cb9ee6fe0d5c76d8b4c25eb9e4f23cbba
Subiramaniam Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-06-27 13:12:38 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,4 +12,10 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'order_product')
+                    ->withPivot('quantity', 'price')
+                    ->withTimestamps();
+    }
 }
