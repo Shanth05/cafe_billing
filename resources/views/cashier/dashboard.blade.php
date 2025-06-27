f712caf865eaa17ea0bac075d34229d025bb8a90
Subiramaniam Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-06-27 19:58:56 +0530

    Update dashboard.blade.php

--- resources/views/cashier/dashboard.blade.php
+++ resources/views/cashier/dashboard.blade.php
@@ -8,7 +8,7 @@
             <p>Process bills and manage daily transactions.</p>
 
             <div class="mt-3 d-flex gap-2">
-                <a href="{{ route('orders.create') }}" class="btn btn-primary">Create New Order</a>
+                <a href="{{ route('cashier.pos') }}" class="btn btn-primary">Create New Order</a>
                 <a href="{{ route('cashier.orders') }}" class="btn btn-secondary">View All Orders</a>
             </div>
         </div>
