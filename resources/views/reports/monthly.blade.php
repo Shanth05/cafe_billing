c6647d4b436b808d1ff3b753283699220be94c83
Subiramaniam Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-06-28 11:39:08 +0530

    Create monthly.blade.php

---
+++ resources/views/reports/monthly.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-4">
+    <h4>Monthly Sales Report - {{ \Carbon\Carbon::parse($month)->format('F Y') }}</h4>
+    <form method="GET">
+        <input type="month" name="month" value="{{ $month }}" onchange="this.form.submit()">
+    </form>
+    <table class="table mt-3">
+        <thead><tr><th>Invoice</th><th>Total</th><th>Date</th></tr></thead>
+        <tbody>
+            @foreach($orders as $order)
+            <tr>
+                <td>{{ $order->invoice_number }}</td>
+                <td>Rs. {{ number_format($order->total, 2) }}</td>
+                <td>{{ $order->created_at->format('Y-m-d H:i') }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <h5>Total: Rs. {{ number_format($total, 2) }}</h5>
+</div>
+@endsection
